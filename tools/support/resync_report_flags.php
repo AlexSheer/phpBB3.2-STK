39f2db40ec4eb136f2dcb7e49d44eb2bc09a2324
AlexSheer <alex_sheer@inbox.lv>
2017-01-09 14:18:26 +0200

    Update resync_report_flags.php

--- tools/support/resync_report_flags.php
+++ tools/support/resync_report_flags.php
@@ -27,14 +27,29 @@ class resync_report_flags
 	 */
 	function display_options()
 	{
-		return 'RESYNC_REPORT_FLAGS';
+		if (@phpversion() < '7.0.0')
+		{
+			return 'RESYNC_REPORT_FLAGS';
+		}
+
+		global $lang;
+
+		if (confirm_box(true))
+		{
+			$this->run_tool();
+		}
+		else
+		{
+			confirm_box(false, user_lang('RESYNC_REPORT_FLAGS_CONFIRM'), '', 'confirm_body.html', STK_DIR_NAME . '/index.' . PHP_EXT . '?c=support&amp;t=resync_report_flags&amp;submit=' . true);
+		}
 	}
 
 	/**
 	 * Run tool
 	 */
 	function run_tool()
-	{		global $request;
+	{
+		global $request;
 
 		$this->mode = $request->variable('m', 'pf');
 
