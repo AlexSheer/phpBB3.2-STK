dc27d43db9569b762b8d116bc8d5b935ebdba032
AlexSheer <alex_sheer@inbox.lv>
2017-01-09 14:22:40 +0200

    Update test.php

--- language/ru/tools/main/test.php
+++ language/ru/tools/main/test.php
@@ -44,6 +44,7 @@ $lang = array_merge($lang, array(
 	'DATABASE_INFO'		=> 'Сервер базы данных',
 	'DBMS'				=> 'Тип базы данных',
 	'PHP_INFO'			=> 'Сведения о php',
+	'PHP_VERSION'		=> 'Версия php',
 	'MBSTRING_LOADED'	=> 'Функции для работы с многобайтными строками (расширение php <b>mbstring</b>) загружены',
 	'MBSTRING_NOT_LOADED'				=> 'Функции для работы с многобайтными строками (расширение php <b>mbstring</b>) не загружены',
 	'ERROR_MBSTRING_NOT_LOADED_EXPLAIN'	=> 'mbstring не входит в список расширений, устанавливаемых по умолчанию. Это значит, что изначально это расширение отключено. Для использования функций этого расширения необходимо явно включить модуль в настройке php. Обратитесь за справкой к <a href="http://php.net/manual/ru/mbstring.configuration.php">документации по php</a>',
